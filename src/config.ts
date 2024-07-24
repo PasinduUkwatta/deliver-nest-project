ed2f6351565ee0ec62580c236cc70580176f7241
pasinduu <u.pasindu@directfn.com>
2024-07-24 15:45:52 +0530

    NEW|ASSIGNEMNT|DELIVER-NEST-PROJECT

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,6 @@
-export default {
+export default  {
   mongo: {
     username:  'upasinduukwatta',
     password: 'VpwB4xI8VLMRbSpZ',
-  },
-  mysql: {
-    username: process.env.MYSQL_USERNAME || 'default_username',
-    password: process.env.MYSQL_PASSWORD || 'default_password',
-    host: process.env.MYSQL_HOST || 'localhost',
-    database: process.env.MYSQL_DATABASE || 'your_database_name',
-  },
+  }
 };
